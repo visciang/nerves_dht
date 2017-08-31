7a9d7ebd423acb983a37a080bf1b11806e0cefcb
Giovanni Visciano <giovanni.visciano@statpro.com>
2017-08-31 08:21:45 +0200

    dht_test.sh default values

--- priv/dht_test.sh
+++ priv/dht_test.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+# Defaults
+DHT_HUMIDITY="${DHT_HUMIDITY:-55.1}"
+DHT_TEMPERATURE="${DHT_TEMPERATURE:-24.719}"
 
 echo "$DHT_HUMIDITY $DHT_TEMPERATURE"
 printf "." >> /tmp/dht_call_count
